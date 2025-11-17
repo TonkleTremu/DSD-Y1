064a9041c98cb08683cadaf35c75e9b77ac2b49a
Tonkle <tonkletremu@gmail.com>
2025-11-17 10:26:22 +0000

    Task 3 completed.

---
+++ Random Test Stuff/ListCompare.py
@@ -0,0 +1,6 @@
+listA = [1, 2, 3] # An integer-exclusive array.
+listB = [1, "2", 3.0] # A mixed data type list.
+
+# Python treats mixed-type lists almost identical to normal lists.
+# Traditional arrays would not allow for mixed data types, and would also not allow for the array to increase in size.
+# Lists are useful for real-world data, since it would likely be constantly increasing, so a fixed array size would not be helpful, and also real-world data may be using many different data types, so a list would be necessary for it.
