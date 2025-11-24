91e86330c6e0a9ff3c0ab9f6a0eb11084fa4d5b0
Tonkle <tonkletremu@gmail.com>
2025-11-24 09:53:26 +0000

    CarSystem.py

---
+++ Random Test Stuff/CarSystem.py
@@ -0,0 +1,9 @@
+HiredDays = int(input("How many days was the car hired for?\n"))
+while(HiredDays <= 0):
+    HiredDays = int(input("How many days was the car hired for? (cannot be zero)\n"))
+MileageStart = float(input("What was the car's initial mileage?\n"))
+MileageEnd = float(input("What was the car's end mileage?\n"))
+MilesDriven = MileageEnd - MileageStart
+Charge = (20 * HiredDays) + (0.05*MilesDriven)
+print(f"The total charge is {Charge}")
+
