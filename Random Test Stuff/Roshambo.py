ad0cdf21f02fc76a97077f84c3f5db56cf46aae3
Tonkle <tonkletremu@gmail.com>
2025-11-24 15:36:46 +0000

    Added Roshambo

---
+++ Random Test Stuff/Roshambo.py
@@ -0,0 +1,14 @@
+import random
+def Roshambo():
+    Choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+    Weaknesses = {"Rock": ["Paper", "Spock"], "Paper": ["Scissors", "Lizard"], "Scissors": ["Rock", "Spock"], "Lizard": ["Rock", "Scissors"], "Spock": ["Lizard", "Paper"]}
+    TheirChoice = random.choice(Choices)
+    YourChoice = input("Rock, Paper, Scissors, Lizard, Spock:\n").capitalize()
+    if(YourChoice in Weaknesses[TheirChoice]):
+        print(f"You win! They chose {TheirChoice} - {YourChoice} beats {TheirChoice}.")
+    elif(YourChoice == TheirChoice):
+        print("Draw!")
+    else:
+        print(f"You lost! They chose {TheirChoice} - {TheirChoice} beats {YourChoice}.")
+    Roshambo()
+Roshambo()
