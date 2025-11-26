283ac85924c8941b75ff7ea94a7be7c1a6a3f044
Tonkle <tonkletremu@gmail.com>
2025-11-26 12:17:29 +0000

    Roshambo Changes

--- Random Test Stuff/Roshambo.py
+++ Random Test Stuff/Roshambo.py
@@ -1,14 +1,22 @@
 import random
+import json
+
 def Roshambo():
-    Choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
-    Weaknesses = {"Rock": ["Paper", "Spock"], "Paper": ["Scissors", "Lizard"], "Scissors": ["Rock", "Spock"], "Lizard": ["Rock", "Scissors"], "Spock": ["Lizard", "Paper"]}
-    TheirChoice = random.choice(Choices)
+    try:
+        f = open("weaknesses", 'r')
+    except:
+        with open('weaknesses', 'w') as f:
+            f.write('{"Rock": ["Paper", "Spock"], "Paper": ["Scissors", "Lizard"], "Scissors": ["Rock", "Spock"], "Lizard": ["Rock", "Scissors"], "Spock": ["Lizard", "Paper"]}')
+        f.close()
+        f = open("weaknesses", 'r')
+    Weaknesses = dict(json.loads(f.read()))
+    TheirChoice = random.choice(Weaknesses.values())
     YourChoice = input("Rock, Paper, Scissors, Lizard, Spock:\n").capitalize()
     if(YourChoice in Weaknesses[TheirChoice]):
         print(f"You win! They chose {TheirChoice} - {YourChoice} beats {TheirChoice}.")
     elif(YourChoice == TheirChoice):
         print("Draw!")
-    elif(YourChoice in Choices):
+    elif(YourChoice in Weaknesses.values()):
         print(f"You lost! They chose {TheirChoice} - {TheirChoice} beats {YourChoice}.")
     else:
         if(random.randint(0,1) == 1):
