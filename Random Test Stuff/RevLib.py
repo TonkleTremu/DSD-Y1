0bd2e6a54dd5afe75baefe8d583c0b95caf3e0c9
Tonkle <tonkletremu@gmail.com>
2025-12-02 16:12:16 +0000

    Revisr Update + Alex told me to import this.

--- Random Test Stuff/RevLib.py
+++ Random Test Stuff/RevLib.py
@@ -5,5 +5,6 @@ OpenEnd = {"What is RAID 0 also known as?": [2, ["stripe set", "striping"]],
            "What is RAID 10?": [2, ["stripe of mirrors", "raid 1+0", "raid 1 + raid 0", "raid 1 + 0", "mirroring+stripe set", "mirroring+striping"]], 
            "What is a PAN?": [2, ["personal area network", "area that covers one person"]], 
            "What is a MAN?": [2, ["metropolitan area network", "a male human", "a city-wide network"]], 
-           "": [0, ["", ""]]} # {Question: [lev_distance, [answer1, answer2]]}
+           "Name two PAN technologies:": [1, ["nfc, rfid", "rfid, nfc", "bluetooth, nfc", "nfc, bluetooth", "rfid, bluetooth", "bluetooth, rfid"]]} # {Question: [lev_distance, [answer1, answer2]]}
+            
 # This is here to copy-paste , "": [0, ["", ""]]
--- Random Test Stuff/Revisr.py
+++ Random Test Stuff/Revisr.py
@@ -1,7 +1,10 @@
 # It's spelt with no 'e' because that's cool.
 import RevLib
 import random
-def AskQuestion(): # Pulls a random question from the library, and asks the user it.
+import time
+import webbrowser
+
+def AskQuestion(askedquestions): # Pulls a random question from the library, and asks the user it.
     question = random.choice(list(RevLib.OpenEnd.items()))
     inputanswer = input(f"{question[0]}\n").lower()
     answers = question[1][1]
@@ -14,7 +17,51 @@ def AskQuestion(): # Pulls a random question from the library, and asks the user
         print("Correct answer!")
     else:
         print(f"Wrong answer. It was {answers[0]}.")
-    AskQuestion()
+    if(askedquestions >= 5):
+        askedquestions = 0
+        Intermissioning()
+    else:
+        askedquestions += 1
+    AskQuestion(askedquestions)
+
+def Intermissioning():
+    Choix = random.randint(0,5)
+    Choix = 5
+    if(Choix == 0):
+        print("We're intermissioning, Pomni!")
+        time.sleep(5)
+    elif(Choix == 1):
+        print("Did you know that fish in Indonesian is 'ikan' - this WILL be on the end-of-year test.")
+        time.sleep(7)
+    elif(Choix == 2):
+        print("You should take a 5-minute break. No, I'm serious. This will not run again until 5 minutes have passed. I mean, you could turn it on and off... but why?")
+        time.sleep(300)
+    elif(Choix == 3):
+        print("Pop-quiz princess: what happens when you bring an element of harmony into another world?")
+        time.sleep(9)
+    elif(Choix == 4):
+        print("This program uses something called Levenshtein distance. It's really cool! Here, I'll open it for you:")
+        time.sleep(4)
+        webbrowser.open("https://www.google.com/search?q=levenshtein+distance/")
+        time.sleep(7)
+    elif(Choix == 5):
+        print("Hey, wanna see me waste your time?")
+        thecount = random.randint(60,6000)
+        for x in range(thecount):
+            print(x)
+            if(x == thecount % 4):
+                print("See, its funny because you have no clue how long this goes on for.")
+            if(x == 2*(thecount % 3)):
+                print("Like, this could be 30% of the way, or 75%. By the way, this could actually go on for like an hour and a half.")
+            if(x == 8*(thecount % 10)):
+                print("In fact, this could only be 20% right now.")
+            if(x == thecount):
+                print("Well... that was fun.")
+            time.sleep(1)
+    else:
+        print("See, I made this one as an else statement. That means this shouldn't run...")
+        time.sleep(3)
+
 
 def Lev(str1, str2):
     m = len(str1)
@@ -42,4 +89,4 @@ def Lev(str1, str2):
     # Return the edit distance between the strings
     return dp[m][n]
 
-AskQuestion()
+AskQuestion(0)
