a56b8ad56fda66b91234f4cac19d2ad6588c2fad
Tonkle <tonkletremu@gmail.com>
2025-12-08 12:21:21 +0000

    updated

--- Random Test Stuff/RevLib.py
+++ Random Test Stuff/RevLib.py
@@ -1,10 +1,16 @@
 OpenEnd = {"What is RAID 0 also known as?": [2, ["stripe set", "striping"]], 
            "What is RAID 1 known as?": [2, ["mirroring"]], 
            "What is RAID 5 known as?": [3, ["striping with parity", "striping + parity", "parity striping"]], 
-           "What is RAID 10?": [2, ["stripe of mirrors", "raid 1+0", "raid 1 + raid 0", "raid 1 + 0", "mirroring+stripe set", "mirroring+striping"]], 
+           "What is RAID 10?": [2, ["stripe of mirrors", "raid 1+0", "raid 1 + raid 0", "raid 1 + 0", "mirroring+stripe set", "mirroring+striping", "striping and mirroring", "mirroring and striping"]], 
            "What is a PAN?": [2, ["personal area network", "area that covers one person"]], 
            "What is a MAN?": [2, ["metropolitan area network", "a male human", "a city-wide network"]], 
            "Name two PAN technologies:": [1, ["nfc, rfid", "rfid, nfc", "bluetooth, nfc", "nfc, bluetooth", "rfid, bluetooth", "bluetooth, rfid"]], 
-           "What does IAAA stand for?": [5, ["identification, authentication, authorisation, accountability", "identification, authentication, accountability, authorisation", "identification, authorisation, authentication, accountability", "identification, authorisation, accountability, authentication", "identification, accountability, authentication, authorisation", "identification, accountability, authorisation, authentication"]]} # {Question: [lev_distance, [answer1, answer2]]}
+           "What does IAAA stand for?": [5, ["identification, authentication, authorisation, accountability", "identification, authentication, accountability, authorisation", "identification, authorisation, authentication, accountability", "identification, authorisation, accountability, authentication", "identification, accountability, authentication, authorisation", "identification, accountability, authorisation, authentication"]], 
+           "What is described by this: 'A device that connects together different networks and forwards data packets along a network.'": [1, ["router", "a router"]], 
+           "Why is pattern recognition useful when making a new program?": [5, ["can reuse code", "you can reuse code", "reuse code", "identify trends", "identify similarities", "reuse design principles", "identify repeated processes", "make predictions"]], 
+           "What does B2C stand for?": [2, ["business to customer"]], 
+           "Name an internal stakeholder.": [2, ["owner", "director", "employee"]], 
+           "Name an external stakeholder.": [3, ["customer", "client", "supplier", "shareholder", "outsourced service", "investor", "funder", "government"]], 
+           "What does SMARTER stand for?": [7, ["specific, measurable, achievable, realistic, time-bound, evaluated, reviewed"]]} # {Question: [lev_distance, [answer1, answer2]]}
 
 # This is here to copy-paste , "": [0, ["", ""]]
