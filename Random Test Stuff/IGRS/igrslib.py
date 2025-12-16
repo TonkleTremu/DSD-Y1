72944f9f0eff99fee872cd40e20e6bd80711c2be
Tonkle <tonkletremu@gmail.com>
2025-12-16 14:22:32 +0000

    children seeker

--- Random Test Stuff/IGRS/igrslib.py
+++ Random Test Stuff/IGRS/igrslib.py
@@ -15,7 +15,20 @@ def CsvToJson():
     with open(FILENAME, "r", newline="") as file:
             reader = csv.reader(file)
             for line in reader:
-                ChildrenInterests.update({"ID": line[0], "Interests": [line[6], line[7]], "Toy": line[4]})
+                ChildrenInterests.update({line[0]: {"Name": line[1], "Interests": [line[6], line[7]], "Toy": line[4]}})
+            ChildrenInterests.pop("child_id")
             print(ChildrenInterests)
 
+def SearchCsv(name): # Returns a list of lists [{ID: {Name, Interests, Toy}}, {ID: {Name, Interests, Toy}}]
+    try:
+        ChosenChildren = []
+        for x in ChildrenInterests:
+             print(ChildrenInterests[x]["Name"])
+             if(ChildrenInterests[x]["Name"] == name.capitalize()):
+                  ChosenChildren.append({x: ChildrenInterests[x]})
+        return(ChosenChildren)
+    except:
+         return("No children found! Santa can't see them while they're sleeping, so he knows they're awake...")
+
 CsvToJson()
+print(SearchCsv(input()))
