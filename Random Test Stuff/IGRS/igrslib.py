d75a3053d6b915f3911458e21a783b524560fa91
Tonkle <tonkletremu@gmail.com>
2025-12-16 11:39:26 +0000

    children.csv

--- Random Test Stuff/IGRS/igrslib.py
+++ Random Test Stuff/IGRS/igrslib.py
@@ -1,8 +1,18 @@
 import csv
 
+FILENAME = "children.csv"
+
 def checkInt(num):
     try:
         num = int(num)
         return True, num
     except:
         return False, 0
+    
+def CsvToJson():
+    with open(FILENAME, "r", newline="") as file:
+            reader = csv.reader(file)
+            for line in reader:
+                print(line)
+
+CsvToJson()
