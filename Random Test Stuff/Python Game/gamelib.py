4adf94e2b6fcb87bcdbdcdea64630ce0b59a38c0
Tonkle <tonkletremu@gmail.com>
2026-01-19 22:52:32 +0000

    20 changes?! never seen it that hi before...

--- Random Test Stuff/Python Game/gamelib.py
+++ Random Test Stuff/Python Game/gamelib.py
@@ -1,13 +1,14 @@
 # Colour Definitions
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
+LIGHT_BLUE = (127, 127, 255)
 MINT = (61, 255, 171)
 
 # Environmental Data
 res_x = 400
 res_y = 300
 game_name = "Test Game"
-FPS = 60
+FPS = 1
 
 # Static Player Data
 PLAYER_HEIGHT = 10
