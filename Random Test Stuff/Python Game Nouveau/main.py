73713eee8285d836e3fdcc6b71e3230b02f0f2ac
Tonkle <tonkletremu@gmail.com>
2026-02-03 12:22:57 +0000

    banana peel

--- Random Test Stuff/Python Game Nouveau/main.py
+++ Random Test Stuff/Python Game Nouveau/main.py
@@ -18,6 +18,8 @@ pygame.init()
 DISPLAYSURF = pygame.display.set_mode((res_x, res_y))
 pygame.display.set_caption("Test")
 fpsClock = pygame.time.Clock()
+worldSizeX = 100
+worldSizeY = 100
 
 
 @dataclass
@@ -35,39 +37,63 @@ class GameObject:
     sprite: Optional[str] = "sprites/error.png" # The object's sprite. If left blank, a shape will be used instead.
 
     # The values for velocity.
+    speed: Optional[float] = 1 # The GameObject's movement speed, used for movement calculations. Higher = faster.
     vel_x: Optional[float] = 0 
     vel_y: Optional[float] = 0
-    rot: Optional[float] = 0 # "rot" is the object's rotational speed. Positive = clockwise. 
 
 def PlayerMovementHandler():
     '''Handles player inputs and such.'''
     # Basic movement script. Accepts either WASD or arrow key inputs.
     if(pygame.key.get_pressed()[K_RIGHT] | pygame.key.get_pressed()[K_d]):
-        player.x += 1
+        player.vel_x += player.speed
     if(pygame.key.get_pressed()[K_LEFT] | pygame.key.get_pressed()[K_a]):
-        player.x -= 1
+        player.vel_x -= player.speed
     if(pygame.key.get_pressed()[K_UP] | pygame.key.get_pressed()[K_w]):
-        player.y -= 1
+        player.vel_y -= player.speed
     if(pygame.key.get_pressed()[K_DOWN] | pygame.key.get_pressed()[K_s]):
-        player.y += 1
+        player.vel_y += player.speed
+    
+    acceleration = 15
+    deceleration = 15
+
+    if(player.vel_x != 0):
+        player.x += player.vel_x / acceleration
+        player.vel_x -= player.vel_x / deceleration
+        if(player.vel_x < 0.5 and player.vel_x > -0.5):
+            player.vel_x = 0
+    if(player.vel_y != 0):
+        player.y += player.vel_y / acceleration
+        player.vel_y -= player.vel_y / deceleration
+        if(player.vel_y < 0.5 and player.vel_y > -0.5):
+            player.vel_y = 0
+        
 
     # When in wall teleport to other wall.
-    if(player.x < player.x_size):
-        player.x = res_x - player.x_size
-    if(player.x > res_x - player.x_size):
-        player.x = player.x_size
-    if(player.y < player.y_size):
-        player.y = res_y - player.y_size
-    if(player.y > res_y - player.y_size):
-        player.y = player.y_size
+    BorderX = 0
+    BorderY = 0
+    if(player.x < BorderX):
+        player.x = worldSizeX - BorderX
+    if(player.x > worldSizeX - BorderX):
+        player.x = BorderX
+    if(player.y < BorderY):
+        player.y = worldSizeY - BorderY
+    if(player.y > worldSizeY - BorderY):
+        player.y = BorderY
     
-    pygame.draw.circle(DISPLAYSURF, MINT, (player.x, player.y), 10, 3)
+    pygame.draw.circle(DISPLAYSURF, MINT, CoordinatesToScreen(player), 10, 3)
 
+def CoordinatesToScreen(Obj):
+    '''Converts a GameObject's co-ordinates to a screen location. Takes the GameObject as a parameter.'''
+    ScalarX = DISPLAYSURF.get_width() / worldSizeX
+    ScalarY = DISPLAYSURF.get_height() / worldSizeY
+    x = round(Obj.x * ScalarX)
+    y = round(Obj.y * ScalarY)
+    return((x,y))
 
 player = GameObject(10, 10)
 
 while True: # Main game loop - like Unity's "update" void thing.
-    DISPLAYSURF.fill(WHITE)
+    DISPLAYSURF.fill(BLACK)
 
     PlayerMovementHandler()
 
