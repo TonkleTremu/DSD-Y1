c54b19f019090d6d3f9cb4e3726f036b06272cc0
Tonkle <tonkletremu@gmail.com>
2026-03-03 16:49:07 +0000

    early

--- Random Test Stuff/GeoCode.py
+++ Random Test Stuff/GeoCode.py
@@ -3,30 +3,33 @@ import json
 
 def FindPlace():
     place_name = input("Enter a place name (or 'quit'):\n").capitalize()
+    
+    if(place_name != "" and place_name != "Quit"):
+        url = f"https://geocoding-api.open-meteo.com/v1/search?name={place_name}"
+        try:
+            # Takes the raw data from the website.
+            raw_data = requests.get(url=url)
 
-    url = f"https://geocoding-api.open-meteo.com/v1/search?name={place_name}"
+            # Converts the hideous raw data into a useful dictionary. [0] exists, because for some reason they store it as a list with one element.
+            data = json.loads(raw_data.text).get("results")[0]
 
-    try:
-        # Takes the raw data from the website.
-        raw_data = requests.get(url=url)
+            print(f"Place: {data.get("name")}, {data.get("country")}")
+            latitude = data.get("latitude")
+            print(f"Latitude: {latitude}")
+            longitude = data.get("longitude")
+            print(f"Longitude: {longitude}")
+            
+            forecast_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m&am…"
+            print(forecast_url)
+            raw_forecast = requests.get(forecast_url)
 
-        # Converts the hideous raw data into a useful dictionary. [0] exists, because for some reason they store it as a list with one element.
-        data = json.loads(raw_data.text).get("results")[0]
-
-        print(f"Place: {data.get("name")}, {data.get("country")}")
-        latitude = data.get("latitude")
-        print(f"Latitude: {latitude}")
-        longitude = data.get("longitude")
-        print(f"Longitude: {longitude}")
-        
-        forecast_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m&am…"
-        print(forecast_url)
-        raw_forecast = requests.get(forecast_url)
-
-        forecast = json.loads(raw_forecast.text).get("results")[0]
-        print(forecast)
-    except:
-        print("Something went wrong. Did you enter a real place name?")
+            forecast = json.loads(raw_forecast.text).get("results")[0]
+            print(forecast)
+        except:
+            print("Something went wrong during runtime. Some stats may not have loaded correctly.")
+    if(place_name == "Quit"):
+        exit()
     FindPlace()
-
+    
+# a horse walks into a bar. the bar says "ow". then, a gnome, elf and dwarf in a trenchcoat approach the horse and ask him if he works there. the horse replies, "no, I just got here". the bar goes up in flames.
 FindPlace()
---
+++ Random Test Stuff/another_data_thing.py
@@ -0,0 +1,73 @@
+import pandas as pd, matplotlib.pyplot as plt, matplotlib.style as style
+
+FILENAME = "Game_Shop_Sales_300_Rows.csv"
+
+plt.style.use("classic")
+
+df = pd.read_csv(FILENAME)
+
+def GamesSales():
+    games = df["Game Title"].unique()
+    total_revenues = df.groupby(["Game Title"])["Total Revenue (£)"].sum()
+    plt.xlabel("Game")
+    plt.ylabel("Total Revenue")
+    plt.bar(games, total_revenues)
+    plt.show()
+
+def GenreSales():
+    games = df["Category"].unique()
+    total_revenues = df.groupby(["Category"])["Total Revenue (£)"].sum()
+    plt.xlabel("Game")
+    plt.ylabel("Total Revenue")
+    plt.bar(games, total_revenues)
+    plt.show()
+
+
+def GameSalesTime():
+    games = df["Game Title"].unique()
+    game_chosen = False
+    while(game_chosen == False):
+        for x in games:
+            print(x)
+        game = input("Which game would you like to check?\n")
+        if(game in games):
+            game_chosen = True
+    dates_and_such = df.loc[df["Game Title"] == game].groupby("Date")
+    total_revenues = dates_and_such["Total Revenue (£)"].sum()
+
+    plt.xlabel("Date")
+    plt.ylabel("Daily Revenue")
+    plt.xticks(rotation=45, ha="right")
+    plt.plot(total_revenues)
+    plt.show()
+
+def GenreSalesTime():
+    genres = df["Category"].unique()
+    genre_chosen = False
+    while(genre_chosen == False):
+        for x in genres:
+            print(x)
+        genre = input("Which genre would you like to check?\n")
+        if(genre in genres):
+            genre_chosen = True
+    dates_and_such = df.loc[df["Category"] == genre].groupby("Date")
+    total_revenues = dates_and_such["Total Revenue (£)"].sum()
+
+    plt.xlabel("Date")
+    plt.ylabel("Daily Revenue")
+    plt.xticks(rotation=45, ha="right")
+    plt.plot(total_revenues)
+    plt.show()
+
+def MainMenu():
+    choix = input("What would you like to do?\n1. Analyse best-selling games.\n2. Check best-selling genres.\n3. View sales of a game over time.\n4. View sales of a genre over time.\n")
+    if(choix == "1"):
+        GamesSales()
+    elif(choix == "2"):
+        GenreSales()
+    elif(choix == "3"):
+        GameSalesTime()
+    elif(choix == "4"):
+        GenreSalesTime()
+    MainMenu()
+MainMenu()
